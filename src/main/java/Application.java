e925a1d82b9a31789b8f2772a99847d3f02d2be3
AndriiShykoriak <andrii.shykoriak@gmail.com>
2020-07-07 16:51:08 +0300

    refactored  return the message that value not present to printReplaceElementOfList and output code to methods

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,30 +2,29 @@ import java.util.*;
 
 public class Application {
     public static void main(String[] args) {
-        List<Integer> listRemove = new ArrayList<>();
-        listRemove.add(3);
-        listRemove.add(9);
-        listRemove.add(11);
-        listRemove.add(18);
-        listRemove.add(20);
-        listRemove.add(22);
-        listRemove.forEach(number -> System.out.print(" " + number));
-        System.out.print("\nremove numbers which are divisible by 3\n");
-        listRemove.removeIf(i -> i % 3 == 0);
-        listRemove.forEach(number -> System.out.print(" " + number));
+        printDeleteElementFromList();
 
-        List<String> fruits = new ArrayList<>();
-        fruits.add("Banana");
-        fruits.add("Apple");
-        fruits.add("Lemon");
-        fruits.add("Orange");
-        fruits.add("Blueberry");
-        System.out.println();
-        fruits.forEach(str -> System.out.print(" " + str));
-        System.out.println("\nreplace Orange on Grapefruit");
-        Collections.replaceAll(fruits, "Orange", "Grapefruit");
-        fruits.forEach(str -> System.out.print(" " + str));
+        printReplaceElementOfList();
 
+        printRetainElementOfLists();
+
+        printSortedElementTreeSetMyComparator();
+    }
+
+    private static void printSortedElementTreeSetMyComparator() {
+        Set<Integer> set = new TreeSet<>(new MyComparator());
+        set.add(1000);
+        set.add(2344);
+        set.add(4563);
+        set.add(34);
+        set.add(456);
+        set.add(5687);
+        set.add(4367);
+        System.out.println("\nPrint DESC TreeSet");
+        set.forEach(num -> System.out.print(" " + num));
+    }
+
+    private static void printRetainElementOfLists() {
         List<Double> doubleList = new ArrayList<>();
         doubleList.add(20d);
         doubleList.add(88.00);
@@ -44,16 +43,37 @@ public class Application {
         System.out.println("\nSimilar elements");
         doubleList.retainAll(doubleListTwo);
         doubleList.forEach(number -> System.out.print(" " + number));
+    }
 
-        Set<Integer> set = new TreeSet<>(new MyComparator());
-        set.add(1000);
-        set.add(2344);
-        set.add(4563);
-        set.add(34);
-        set.add(456);
-        set.add(5687);
-        set.add(4367);
-        System.out.println("\nPrint DESC TreeSet");
-        set.forEach(num -> System.out.print(" " + num));
+    private static void printReplaceElementOfList() {
+        List<String> fruits = new ArrayList<>();
+        fruits.add("Banana");
+        fruits.add("Apple");
+        fruits.add("Lemon");
+        fruits.add("Oranges");
+        fruits.add("Blueberry");
+        System.out.println();
+        fruits.forEach(str -> System.out.print(" " + str));
+        System.out.println("\nreplace Orange on Grapefruit");
+        if (fruits.contains("Orange")) {
+            Collections.replaceAll(fruits, "Orange", "Grapefruit");
+            fruits.forEach(str -> System.out.print(" " + str));
+        } else {
+            System.out.println("value not present");
+        }
+    }
+
+    private static void printDeleteElementFromList() {
+        List<Integer> listRemove = new ArrayList<>();
+        listRemove.add(3);
+        listRemove.add(9);
+        listRemove.add(11);
+        listRemove.add(18);
+        listRemove.add(20);
+        listRemove.add(22);
+        listRemove.forEach(number -> System.out.print(" " + number));
+        System.out.print("\nremove numbers which are divisible by 3\n");
+        listRemove.removeIf(i -> i % 3 == 0);
+        listRemove.forEach(number -> System.out.print(" " + number));
     }
 }
